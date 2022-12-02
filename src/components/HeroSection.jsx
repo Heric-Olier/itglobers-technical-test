a2776182104416c4127fcd4d640f8d37eade9989
Heric Olier <hericolier.code@gmail.com>
2022-12-02 12:24:38 -0500

    Add Carousel Hero Section [ Swiperjs ]

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,22 +1,36 @@
-import heroimage1 from "../assets/heroimage1.jpg";
-import heroimage2 from "../assets/heroimage2.jpg";
-import heroimage3 from "../assets/heroimage3.jpg";
 import { NavBar } from "../components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper";
+import { Navigation, Pagination, Autoplay, Lazy } from "swiper";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
 export const HeroSection = () => {
+  const slides = [
+    {
+      id: 1,
+      image:
+        "https://raw.githubusercontent.com/Heric-Olier/images-repository/main/heroImage1.jpg?token=GHSAT0AAAAAABWFNSCKPWAMM6XK2RKCA2MGY4KIDRQ",
+    },
+    {
+      id: 2,
+      image:
+        "https://raw.githubusercontent.com/Heric-Olier/images-repository/main/heroImage2.jpg?token=GHSAT0AAAAAABWFNSCK5L44ARHJ24WRUIFCY4KIEKA",
+    },
+    {
+      id: 3,
+      image:
+        "https://raw.githubusercontent.com/Heric-Olier/images-repository/main/heroImage3.jpg?token=GHSAT0AAAAAABWFNSCLUMY2BK3KNF6UTW56Y4KIGWA",
+    },
+  ];
+
   return (
     <div id="hero-section">
       <NavBar />
       <div className="hero-section__container">
         <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
+          modules={[Navigation, Pagination, Autoplay, Lazy]}
           spaceBetween={50}
           slidesPerView={1}
           loop
@@ -25,21 +39,16 @@ export const HeroSection = () => {
           autoplay={{ delay: 4000 }}
           style={{ width: "100%", height: "100%" }}
         >
-          <SwiperSlide style={{ display: "flex", justifyContent: "center" }}>
-            <div className="hero-section__container--image">
-              <img src={heroimage1} alt="heroimage1" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide style={{ display: "flex", justifyContent: "center" }}>
-            <div className="hero-section__container--image">
-              <img src={heroimage2} alt="heroimage2" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide style={{ display: "flex", justifyContent: "center" }}>
-            <div className="hero-section__container--image">
-              <img src={heroimage3} alt="heroimage3" />
-            </div>
-          </SwiperSlide>
+          {slides.map((slide) => (
+            <SwiperSlide
+              key={slide.id}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              <div className="hero-section__container--image">
+                <img src={slide.image} alt="hero image" />
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
         <div className="hero-section__container--text">
           <h4>
